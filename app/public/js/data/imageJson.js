9972f300b0d4b7f167be9d91051eec4d06a45fd4
Side Li <lisided@gmail.com>
2016-04-22 22:05:20 -0700

    added science collection

--- app/public/js/data/imageJson.js
+++ app/public/js/data/imageJson.js
@@ -210,243 +210,344 @@ var defaultImg = [
 
 var genderImg = [
   {
+      text: "Please select a pattern",
+      value: 0,
+      selected: false,
+      imageSrc: "/../../img/default.jpg"
+  },
+  {
       text: "Blue",
       value: "blue_",
       selected: true,
-      imageSrc: "/../../img/blue_.jpg"
+      imageSrc: "/../../img/gender/blue_.jpg"
   },
   {
       text: "Blue Female",
       value: "blue_Female",
       selected: true,
-      imageSrc: "/../../img/blue_Female.jpg"
+      imageSrc: "/../../img/gender/blue_Female.jpg"
   },
   {
       text: "Bule Male",
       value: "blue_Male",
       selected: true,
-      imageSrc: "/../../img/blue_Male.jpg"
+      imageSrc: "/../../img/gender/blue_Male.jpg"
   },
   {
       text: "Cyan",
       value: "cyan_",
       selected: true,
-      imageSrc: "/../../img/cyan_.jpg"
+      imageSrc: "/../../img/gender/cyan_.jpg"
   },
   {
       text: "Cyan Female",
       value: "cyan_Female",
       selected: true,
-      imageSrc: "/../../img/cyan_Female.jpg"
+      imageSrc: "/../../img/gender/cyan_Female.jpg"
   },
   {
       text: "Cyan Male",
       value: "cyan_Male",
       selected: true,
-      imageSrc: "/../../img/cyan_Male.jpg"
+      imageSrc: "/../../img/gender/cyan_Male.jpg"
   },
   {
       text: "Darkgreen",
       value: "darkgreen_",
       selected: true,
-      imageSrc: "/../../img/darkgreen_.jpg"
+      imageSrc: "/../../img/gender/darkgreen_.jpg"
   },
   {
       text: "Darkgreen Female",
       value: "darkgreen_Female",
       selected: true,
-      imageSrc: "/../../img/darkgreen_Female.jpg"
+      imageSrc: "/../../img/gender/darkgreen_Female.jpg"
   },
   {
       text: "Darkgreen Male",
       value: "darkgreen_Male",
       selected: true,
-      imageSrc: "/../../img/darkgreen_Male.jpg"
+      imageSrc: "/../../img/gender/darkgreen_Male.jpg"
   },
   {
       text: "Darkyellow",
       value: "darkyellow_",
       selected: true,
-      imageSrc: "/../../img/darkyellow_.jpg"
+      imageSrc: "/../../img/gender/darkyellow_.jpg"
   },
   {
       text: "Darkyellow Female",
       value: "darkyellow_Female",
       selected: true,
-      imageSrc: "/../../img/darkyellow_Female.jpg"
+      imageSrc: "/../../img/gender/darkyellow_Female.jpg"
   },
   {
       text: "Darkyellow Male",
       value: "darkyellow_Male",
       selected: true,
-      imageSrc: "/../../img/darkyellow_Male.jpg"
+      imageSrc: "/../../img/gender/darkyellow_Male.jpg"
   },
   {
       text: "Gray",
       value: "gray_",
       selected: true,
-      imageSrc: "/../../img/gray_.jpg"
+      imageSrc: "/../../img/gender/gray_.jpg"
   },
   {
       text: "Gray Female",
       value: "gray_Female",
       selected: true,
-      imageSrc: "/../../img/gray_Female.jpg"
+      imageSrc: "/../../img/gender/gray_Female.jpg"
   },
   {
       text: "Gray Male",
       value: "gray_Male",
       selected: true,
-      imageSrc: "/../../img/gray_Male.jpg"
+      imageSrc: "/../../img/gender/gray_Male.jpg"
   },
   {
       text: "Green",
       value: "green_",
       selected: true,
-      imageSrc: "/../../img/green_.jpg"
+      imageSrc: "/../../img/gender/green_.jpg"
   },
   {
       text: "Green Female",
       value: "green_Female",
       selected: true,
-      imageSrc: "/../../img/green_Female.jpg"
+      imageSrc: "/../../img/gender/green_Female.jpg"
   },
   {
       text: "Green Male",
       value: "green_Male",
       selected: true,
-      imageSrc: "/../../img/green_Male.jpg"
+      imageSrc: "/../../img/gender/green_Male.jpg"
   },
   {
       text: "Magenta",
       value: "magenta_",
       selected: true,
-      imageSrc: "/../../img/magenta_.jpg"
+      imageSrc: "/../../img/gender/magenta_.jpg"
   },
   {
       text: "Magenta Female",
       value: "magenta_Female",
       selected: true,
-      imageSrc: "/../../img/magenta_Female.jpg"
+      imageSrc: "/../../img/gender/magenta_Female.jpg"
   },
   {
       text: "Magenta Male",
       value: "magenta_Male",
       selected: true,
-      imageSrc: "/../../img/magenta_Male.jpg"
+      imageSrc: "/../../img/gender/magenta_Male.jpg"
   },
   {
       text: "Organge",
       value: "orange_",
       selected: true,
-      imageSrc: "/../../img/orange_.jpg"
+      imageSrc: "/../../img/gender/orange_.jpg"
   },
   {
       text: "Organge Female",
       value: "orange_Female",
       selected: true,
-      imageSrc: "/../../img/orange_Female.jpg"
+      imageSrc: "/../../img/gender/orange_Female.jpg"
   },
   {
       text: "Organge Male",
       value: "orange_Male",
       selected: true,
-      imageSrc: "/../../img/orange_Male.jpg"
+      imageSrc: "/../../img/gender/orange_Male.jpg"
   },
   {
       text: "Pink",
       value: "pink_",
       selected: true,
-      imageSrc: "/../../img/pink_.jpg"
+      imageSrc: "/../../img/gender/pink_.jpg"
   },
   {
       text: "Pink Female",
       value: "pink_Female",
       selected: true,
-      imageSrc: "/../../img/pink_Female.jpg"
+      imageSrc: "/../../img/gender/pink_Female.jpg"
   },
   {
       text: "Pink Male",
       value: "pink_Male",
       selected: true,
-      imageSrc: "/../../img/pink_Male.jpg"
+      imageSrc: "/../../img/gender/pink_Male.jpg"
   },
   {
       text: "Red",
       value: "red_",
       selected: true,
-      imageSrc: "/../../img/red_.jpg"
+      imageSrc: "/../../img/gender/red_.jpg"
   },
   {
       text: "Red Female",
       value: "red_Female",
       selected: true,
-      imageSrc: "/../../img/red_Female.jpg"
+      imageSrc: "/../../img/gender/red_Female.jpg"
   },
   {
       text: "Red Male",
       value: "red_Male",
       selected: true,
-      imageSrc: "/../../img/red_Male.jpg"
+      imageSrc: "/../../img/gender/red_Male.jpg"
   },
   {
       text: "Steel",
       value: "steel_",
       selected: true,
-      imageSrc: "/../../img/steel_.jpg"
+      imageSrc: "/../../img/gender/steel_.jpg"
   },
   {
       text: "Steel Female",
       value: "steel_Female",
       selected: true,
-      imageSrc: "/../../img/steel_Female.jpg"
+      imageSrc: "/../../img/gender/steel_Female.jpg"
   },
   {
       text: "Steel Male",
       value: "steel_Male",
       selected: true,
-      imageSrc: "/../../img/steel_Male.jpg"
+      imageSrc: "/../../img/gender/steel_Male.jpg"
   },
   {
       text: "Violet",
       value: "violet_",
       selected: true,
-      imageSrc: "/../../img/violet_.jpg"
+      imageSrc: "/../../img/gender/violet_.jpg"
   },
   {
       text: "Violet Female",
       value: "violet_Female",
       selected: true,
-      imageSrc: "/../../img/violet_Female.jpg"
+      imageSrc: "/../../img/gender/violet_Female.jpg"
   },
   {
       text: "Violet Male",
       value: "violet_Male",
       selected: true,
-      imageSrc: "/../../img/violet_Male.jpg"
+      imageSrc: "/../../img/gender/violet_Male.jpg"
   },
   {
       text: "Yellow",
       value: "yellow_",
       selected: true,
-      imageSrc: "/../../img/yellow_.jpg"
+      imageSrc: "/../../img/gender/yellow_.jpg"
   },
   {
       text: "Yellow Female",
       value: "yellow_Female",
       selected: true,
-      imageSrc: "/../../img/yellow_Female.jpg"
+      imageSrc: "/../../img/gender/yellow_Female.jpg"
   },
   {
       text: "Yellow Male",
       value: "yellow_Male",
       selected: true,
-      imageSrc: "/../../img/yellow_Male.jpg"
+      imageSrc: "/../../img/gender/yellow_Male.jpg"
   }
 ];
 
 var scienceImg = [
-
+  {
+      text: "Please select a pattern",
+      value: 0,
+      selected: false,
+      imageSrc: "/../../img/default.jpg"
+  },
+  {
+      text: "Airplane I",
+      value: "airplane47",
+      selected: true,
+      imageSrc: "/../../img/science/airplane47.jpg"
+  },
+  {
+      text: "Airplane II",
+      value: "airplane48",
+      selected: true,
+      imageSrc: "/../../img/science/airplane48.jpg"
+  },
+  {
+      text: "Airplane III",
+      value: "airplane49",
+      selected: true,
+      imageSrc: "/../../img/science/airplane49.jpg"
+  },
+  {
+      text: "Astronaut",
+      value: "astronaut",
+      selected: true,
+      imageSrc: "/../../img/science/astronaut.jpg"
+  },
+  {
+      text: "Atom I",
+      value: "atom6",
+      selected: true,
+      imageSrc: "/../../img/science/atom6.jpg"
+  },
+  {
+      text: "Atom II",
+      value: "atom7",
+      selected: true,
+      imageSrc: "/../../img/science/atom7.jpg"
+  },
+  {
+      text: "Axe",
+      value: "axe3",
+      selected: true,
+      imageSrc: "/../../img/science/axe3.jpg"
+  },
+  {
+      text: "Barrel",
+      value: "barrel",
+      selected: true,
+      imageSrc: "/../../img/science/barrel.jpg"
+  },
+  {
+      text: "Barrels",
+      value: "barrels",
+      selected: true,
+      imageSrc: "/../../img/science/barrels.jpg"
+  },
+  {
+      text: "Battery I",
+      value: "battery79",
+      selected: true,
+      imageSrc: "/../../img/science/battery79.jpg"
+  },
+  {
+      text: "Battery II",
+      value: "battery80",
+      selected: true,
+      imageSrc: "/../../img/science/battery80.jpg"
+  },
+  {
+      text: "Battery III",
+      value: "battery81",
+      selected: true,
+      imageSrc: "/../../img/science/battery81.jpg"
+  },
+  {
+      text: "Battery IV",
+      value: "battery82",
+      selected: true,
+      imageSrc: "/../../img/science/battery82.jpg"
+  },
+  {
+      text: "Battery V",
+      value: "battery83",
+      selected: true,
+      imageSrc: "/../../img/science/battery83.jpg"
+  },
+  {
+      text: "Battery VI",
+      value: "battery84",
+      selected: true,
+      imageSrc: "/../../img/science/battery84.jpg"
+  },
 ];
 
 function generateImgJson(options){
