131917e910de8bedfd6bed7d882c903b7e4526b1
liside <lisided@gmail.com>
2017-03-04 11:13:57 -0800

    added check block before users upload their files

--- app/public/js/views/home.js
+++ app/public/js/views/home.js
@@ -6,11 +6,20 @@ $(document).ready(function(){
 
 	$('#new-survey').ajaxForm({
 		beforeSubmit : function(formData, jqForm, options){
-			var name = document.getElementById('new-file-name').value + ".csv";
-			$('.modal-loading').modal({ show : false, keyboard : false, backdrop : 'static' });
-			$('.modal-loading .modal-body h3').html('Loading....');
-			$('.modal-loading').modal('show');
-			return true;
+			// check if uploaded file is of type text/csv
+			if (formData[0].value.type != 'text/csv') {
+				$('.modal-alert').modal('toggle');
+				// $('.modal-alert .modal-header h3').text('Warning!');
+				$('.modal-alert p').html("Please upload a csv file!");
+				return false;
+			} else {
+				var name = document.getElementById('new-file-name').value + ".csv";
+				$('.modal-loading').modal({ show : false, keyboard : false, backdrop : 'static' });
+				$('.modal-loading .modal-body h3').html('Loading....');
+				$('.modal-loading').modal('show');
+				return true;
+			}
+
 		},
 		success	: function(responseText, status, xhr, $form){
 			setTimeout(function(){window.location.href = '/';}, 300);
